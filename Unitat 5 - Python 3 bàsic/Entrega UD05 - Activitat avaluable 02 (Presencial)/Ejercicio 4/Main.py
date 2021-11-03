603ea0601188463b8a709902f743c6a2ceb44aae
Federico Adria <fadriacarrasco@gmail.com>
2021-11-03 17:15:00 +0100

    Entrega 2 comienzo ejercicio 4

---
+++ Unitat 5 - Python 3 bàsic/Entrega UD05 - Activitat avaluable 02 (Presencial)/Ejercicio 4/Main.py
@@ -0,0 +1,7 @@
+# Función usada para contar las minas adyacentes en cada posición del array
+def contandoMinas(miCampo):
+    return miCampo
+
+# Array que contiene nuestro buscaminas, los valores -1 nos indican que en esa posición, tendríamos una mina
+arrayBidimensional = [(0, 0, -1, 0), (0, -1, -1, 0)]
+
