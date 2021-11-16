abea1a5a3e0f00171051ce5e874deec22006adda
Federico Adria <fadriacarrasco@gmail.com>
2021-11-16 12:11:05 +0100

    Ejercicio 9

---
+++ Unitat 5 - Python 3 bàsic/Entrega UD05 - Activitat avaluable 02 (Presencial)/Ejercicio 9/Main.py
@@ -0,0 +1,18 @@
+import pandas as pd # Liberaría para leer ficheros csv
+import sys # Liberaría usada para leer parámetros desde consola
+
+import barcode # Librería del código de barras
+from barcode import EAN13 # Librería para generar un código de barras en formato EAN13
+from barcode.writer import ImageWriter # Librería usada para generar la imagen del código de barras
+
+nombreFichero = sys.argv[1] # Variable que contiene el nombre del fichero csv a leer
+
+df2 = pd.read_csv(nombreFichero, header=None) # Leemos el fichero
+listadoNombres = df2[0] # La primera columna contiene los nombres
+listadoCodigos = df2[1] # La segunda columna contiene los números
+
+#Cremos un for que con zip recorre dos listas a la vez
+for i in range(len(listadoNombres)):
+    EAN = barcode.get_barcode_class('ean13') # Obtenemos la clase correspondiente al EAN13
+    ean = EAN(str(listadoCodigos[i]), writer=ImageWriter()) # Variable para el código EAN de cada alumno
+    nombreFichero = ean.save(str(listadoNombres[i])) # Generamos la imagen de cada código
