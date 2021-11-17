9bf8c0b8060414ce31910bf56f0ef5e9416910ec
Federico Adria <fadriacarrasco@gmail.com>
2021-11-17 16:44:09 +0100

    Ejercicio 13 finalizado

--- Unitat 5 - Python 3 bàsic/Entrega UD05 - Activitat avaluable 02 (Presencial)/Ejercicio 13/Main.py
+++ Unitat 5 - Python 3 bàsic/Entrega UD05 - Activitat avaluable 02 (Presencial)/Ejercicio 13/Main.py
@@ -16,9 +16,21 @@ data = requests.get(URL)
 
 data = data.json() # Convertimos la respuesta recibida en un diccionario
 
-try:
-    print("\nListado de personajes de la especie " + str(especie) + ":")
-    for element in data["results"]: #iteramos sobre data
-        print(element['name']) #Accedemos a sus valores
-except:
-    print("No se han encontrado personajes de la especie " + str(especie))
+'''
+    Al realizar la petición a la API, obtenemos un número total de páginas. Esto se hace para no devolver 1000 personajes de golpe,
+    por ejemplo. Por ello, haremos un bucle for que realice la petición para obtener los personajes de cada página y los mostraremos.
+'''
+numeroPaginas = data["info"]["pages"]
+
+
+print("\nListado de personajes de la especie " + str(especie) + ":")
+
+for i in range(1, numeroPaginas+1): # Mostraremos desde la página 1 hasta la última + 1, que correspondería a la última disponible
+    data = requests.get(URL + "&page=" + str(i)) # Obtenemos la tira de personajes que corresponda en cada itineración
+    data = data.json() # Convertimos la respuesta recibida en un diccionario
+
+    try:
+        for element in data["results"]: # Obtenemos el apartado results dentro de la respuesta de la API
+            print(element['name']) #Accedemos a sus valores
+    except:
+        print("Se ha producido un error")
