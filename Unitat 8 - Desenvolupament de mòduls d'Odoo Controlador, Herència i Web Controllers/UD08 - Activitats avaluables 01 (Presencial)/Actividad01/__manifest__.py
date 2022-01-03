8e80dbd607efac6351d00c6fdb6bd5533a556866
Federico Adria <fadriacarrasco@gmail.com>
2022-01-03 16:13:04 +0100

    Ejercicio 1 apartado 1

---
+++ Unitat 8 - Desenvolupament de mòduls d'Odoo Controlador, Herència i Web Controllers/UD08 - Activitats avaluables 01 (Presencial)/Actividad01/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ejercicio 01 Entrega 02",  # Titulo del módulo
+    'summary': "Gestionar una liga de futbol :) (Version avanzada)",  # Resumen de la funcionaliadad
+    'description': """
+    Gestor de Liga de futbol (Version avanzada)
+    ==============
+    """,  
+
+    #Indicamos que es una aplicación
+    'application': True,
+    'author': "Federico Adria",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+
+    'data': [
+
+      
+        #Estos dos primeros ficheros:
+        #1) El primero indica grupo de seguridad basado en rol
+        #2) El segundo indica la politica de acceso del modelo
+        #Mas información en https://www.odoo.com/documentation/14.0/es/developer/howtos/rdtraining/05_securityintro.html
+        #Y en www.odoo.yenthevg.com/creating-security-groups-odoo/
+        #'security/groups.xml',
+        'security/ir.model.access.csv',
+
+        #Aqui distintas vistas de equipo (vistas diferentes, mismo modelo)
+        'views/liga_equipo.xml',
+        'views/liga_equipo_clasificacion.xml',
+        #Vista a un informe
+        'report/liga_equipo_clasificacion_report.xml',
+        #Aqui vista sobre los partidos
+        'views/liga_partido.xml',
+        #Añadimos un Wizard para introducir equipos
+        'wizard/liga_equipo_wizard.xml'
+        
+    ],
+    # Fichero con data de demo si se inicializa la base de datos con "demo data" (No incluido en ejemplo)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
