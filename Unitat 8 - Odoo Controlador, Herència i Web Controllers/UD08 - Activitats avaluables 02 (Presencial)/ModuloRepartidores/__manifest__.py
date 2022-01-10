015c3459663a3aba158e09b906b40ee1cac7d9c6
Federico Adria <fadriacarrasco@gmail.com>
2022-01-10 11:20:38 +0100

    Modelo empleados

---
+++ Unitat 8 - Odoo Controlador, Herència i Web Controllers/UD08 - Activitats avaluables 02 (Presencial)/ModuloRepartidores/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Empresa de transportes",  # Titulo del módulo
+    'summary': "Módulo destinado a ser utilizado por empresas de transportes para controlar los diferentes datos de ella",  # Resumen de la funcionaliadad
+    'description': """
+    Módulo empresa de transportes
+    ==============
+    """,  
+
+    #Indicamos que es una aplicación
+    'application': True,
+    'author': "Federico Adria",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+
+    'data': [      
+        'security/ir.model.access.csv',
+
+        #Aqui distintas vistas de equipo (vistas diferentes, mismo modelo)
+        'views/empleados.xml',
+    ],
+    # Fichero con data de demo si se inicializa la base de datos con "demo data" (No incluido en ejemplo)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
---
+++ Unitat 8 - Odoo Controlador, Herència i Web Controllers/UD08 - Activitats avaluables 02 (Presencial)/ModuloRepartidores/models/empleados.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class Empleados(models.Model):
+    # Nombre y descripcion del modelo
+    _name = 'empleados'
+    _description = 'Empleados de la empresa de transporte'
+
+    # Parametros de ordenacion por defecto
+    _order = 'nombre'
+
+    # ATRIBUTOS
+
+    # PARA CUANDO NO HAY UN ATRIBUTO LLAMADO NAME PARA MOSTRAR NOMBRE DE UN REGISTRO
+    # https://www.odoo.com/es_ES/forum/ayuda-1/how-defined-display-name-in-custom-many2one-91657
+    
+    # Indicamos que atributo sera el que se usara para mostrar nombre.
+    # Por defecto es "name", pero si no hay un atributo que se llama name, aqui lo indicamos
+    # Aqui indicamos que se use el atributo "nombre"
+    _rec_name = 'nombre'    
+
+    #Elementos de cada fila del modelo de datos
+    #Los tipos de datos a usar en el ORM son 
+    # https://www.odoo.com/documentation/14.0/developer/reference/addons/orm.html#fields
+
+    nombre = fields.Char("Nombre")
+    apellidos = fields.Char("Apellidos")
+    dni = fields.Char("DNI")
+    telefono = fields.Char("Teléfono")
+    carnet_ciclomotor = fields.Boolean("Carnet de ciclomotor")
+    carnet_furgoneta = fields.Boolean("Carnet de furgoneta")
+    foto = fields.Image('Foto Empleado',max_width=50,max_height=50)
