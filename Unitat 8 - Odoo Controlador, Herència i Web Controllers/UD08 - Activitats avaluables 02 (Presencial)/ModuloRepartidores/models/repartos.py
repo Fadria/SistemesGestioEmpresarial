ad0c232eefb383ee0b93a1e2f38703ca7fb6486a
Federico Adria <fadriacarrasco@gmail.com>
2022-01-12 09:54:10 +0100

    Comprobación de la validez del vehículo según la cantidad de kilómetros

--- Unitat 8 - Odoo Controlador, Herència i Web Controllers/UD08 - Activitats avaluables 02 (Presencial)/ModuloRepartidores/models/repartos.py
+++ Unitat 8 - Odoo Controlador, Herència i Web Controllers/UD08 - Activitats avaluables 02 (Presencial)/ModuloRepartidores/models/repartos.py
@@ -46,3 +46,22 @@ class Repartos(models.Model):
     estado = fields.Selection(ESTADOSREPARTO, default=ESTADOSREPARTO[0][0])
     emisor = fields.Many2one("clientes", "Emisor")
     receptor = fields.Many2one("clientes", "Receptor")
+
+
+    # Indicamos que esta funcion es una "Constraints" de ese atributo
+    # Dicho de otra forma, cada vez que se cambie ese atributo, se lanzara esta funcion
+    # Y si la funcion detecta un cambio inadecuado, cambiara una instruccion
+    # Util cuando la constraint no se puede definir con sintaxis SQL y debe indicar en una funcion
+    @api.constrains('vehiculo')
+    def _check_validez_vehiculo(self):
+        # Recorremos el modelo
+        for record in self:
+            # Comprobamos que los repartos de más de 10 kilómetros no se puedan hacer con ciclomotor
+            if record.kilometros > 10 and record.vehiculo.tipo == "ciclomotor":
+                # Levantamos un error para informar al usuario
+                raise models.ValidationError('Los repartos de más de 10 kilómetros no se pueden realizar en ciclomotor.')
+
+            # Comprobamos que los repartos de menos de 1 kilómetro no se puedan hacer con furgoneta
+            elif record.kilometros < 1 and record.vehiculo.tipo == "furgoneta":
+                # Levantamos un error para informar al usuario
+                raise models.ValidationError('Los repartos de menos de 1 kilómetro no se pueden realizar en furgoneta.')
